de20d5193c3dd303c6a6596fc2449048d5cebce7
Oblivious <me262thanos@gmail.com>
2023-02-18 20:22:01 +0200

    Update index.svelte

--- src/index.svelte
+++ src/index.svelte
@@ -22,7 +22,7 @@
     </div>
     
     <p class="read-the-docs">
-        Click on the Vite and Svelte logos to learn more      okokokokokokokokokok
+        Click on the Vite and Svelte logos to learn more
     </p>
 </main>
 
