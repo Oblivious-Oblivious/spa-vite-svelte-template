51e5c4ae8dd9a9e0fc478ba9c2b72468c3075cb9
Oblivious <thanos.papap42@gmail.com>
2023-02-19 03:04:08 +0200

    Renamed `index` for js and svelte to `app`.

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import "./styles.css";
+import app from "./app.svelte";
+
+export default new app({
+    target: document.getElementById("app"),
+});
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import "./styles.css";
-import index from "./index.svelte";
-
-export default new index({
-    target: document.getElementById("index"),
-});
